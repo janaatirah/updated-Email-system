c5c0d6be0f03f4931227e59424f5ceac3fce2b2c
janaatirah <121237400+janaatirah@users.noreply.github.com>
2022-12-27 17:50:14 +0530

    register updation

--- src/Views/index.jsx
+++ src/Views/index.jsx
@@ -2,10 +2,11 @@ import { Password } from "@mui/icons-material";
 import   * as Yup  from "yup";
 export const  signUpSchema= Yup.object(
     {
-        firstName:Yup.string().min(5).max(10).required("enter"),
-        lastName:Yup.string().min(5).max(10).required("ur"),
-        email : Yup.string().required().email().label('Email'),
-        password:Yup.string().min(5).max(10).required("djldkshdf"),
-        // confirmPassword:Yup.string().required.oneOf([Yup.ref('password'),
-        // null],"match")
+        firstName:Yup.string().min(5).max(10).required("Please Enter your first name"),
+        lastName:Yup.string().min(5).max(10).required("Please Enter your last name"),
+        email : Yup.string().required().email().label('Email required'),
+        password:Yup.string().min(5).max(10).required("Enter your password"),
+        confirmPassword: Yup.string()
+      .required('Confirm your Password first')
+      .oneOf([Yup.ref('password')], 'Passwords does not match'),
     });
