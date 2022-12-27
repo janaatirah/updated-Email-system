54fc2fea3e09978705628028fa349b4b5833a319
janaatirah <121237400+janaatirah@users.noreply.github.com>
2022-12-27 19:58:28 +0530

    register

--- src/Views/index.jsx
+++ src/Views/index.jsx
@@ -4,7 +4,7 @@ export const  signUpSchema= Yup.object(
     {
         firstName:Yup.string().min(5).max(10).required("Please Enter your first name"),
         lastName:Yup.string().min(5).max(10).required("Please Enter your last name"),
-        email : Yup.string().required().email().label('Email required'),
+        email : Yup.string().required().email(),
         password:Yup.string().min(5).max(10).required("Enter your password"),
         confirmPassword: Yup.string()
       .required('Confirm your Password first')
