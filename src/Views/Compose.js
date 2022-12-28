d711cc0d9dc41b24d508de180fd324aa49ece80b
janaatirah <121237400+janaatirah@users.noreply.github.com>
2022-12-28 19:40:58 +0530

    update  compose

--- src/Views/Compose.js
+++ src/Views/Compose.js
@@ -4,26 +4,24 @@ import { Button, Card, CardContent, Grid, TextField, Typography } from '@mui/mat
 export default function Compose(){
     return(
 
-        <Card style={{maxWidth:400, margin:"0 auto", padding:"20px 30px"}}>
+        <Card style={{maxWidth:400, margin:"2px auto", padding:"60px 30px"}}>
             <CardContent>
                 <Typography gutterBottom variant="h5">Compose Email</Typography>
                 <Typography gutterBottom color="textSecondary" variant="body2" component="p">Please Fill up the form!</Typography>
                 <form>
-                <Grid container sapcing={1}>
+                <Grid container spacing={2}>
 
-                    <Grid xs={10}  item>
-                     <TextField  label="Full Name" placeholder="Enter full name" variant="filled" fullWidth required/>                        
+                    
+                    <Grid  xs={18}  item>
+                     <TextField type="email"  label="Recipient" placeholder=" Enter email" variant="filled" fullWidth required/>                        
                     </Grid>
-                    <Grid  xs={10}  item>
-                     <TextField type="email"  label="Email" placeholder=" Enter email" variant="filled" fullWidth required/>                        
+                    <Grid xs={18}  item>
+                     <TextField type="text"  label="Subject" variant="filled" fullWidth required/>                        
                     </Grid>
-                    <Grid xs={10}  item>
-                     <TextField type="number"  label="Phone" placeholder=" Enter phone number" variant="filled" fullWidth required/>                        
-                    </Grid>
-                    <Grid xs={10}   item>
+                    <Grid xs={18}   item>
                      <TextField   label="Message" multiline rows={4} placeholder=" Enter your message" variant="filled" fullWidth required/>                        
                     </Grid>
-                    <Grid xs={10}  item>
+                    <Grid xs={5} item>
                       <Button type="submit" variant="contained" color="primary" fullWidth>Send</Button>  
                     </Grid>
                 </Grid>
