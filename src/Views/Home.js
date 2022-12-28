48f1cd2dcc39eda2aedcec4ad9b4d99b38e58298
waniminah <112078487+waniminah@users.noreply.github.com>
2022-12-28 06:54:06 -0800

    updated home

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,27 +1,25 @@
 import * as React from 'react';
-
+import { Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material';
 
 export default function Home (){
   return(
        
-    <div class="login-box">
-      <div>
+    <Card style={{maxWidth:400, margin:"60px auto", padding:"60px 30px"}}>
+    <CardContent>
+        <Typography gutterBottom variant="h5">Search Account</Typography>
+        <Typography gutterBottom color="textSecondary" variant="body2" component="p">Please enter email!</Typography>
+        <form>
+        <Grid container spacing={2}>            
+            <Grid  xs={18}  item>
+             <TextField type="email"  label="Email" placeholder=" Enter email" variant="filled" fullWidth required/>                        
+            </Grid>
+            <Grid xs={5} item>
+              <Button type="submit" variant="contained" color="primary" fullWidth>Send</Button>  
+            </Grid>
+        </Grid>
+        </form>
+    </CardContent>
+</Card>
 
-      <h2>Search for user</h2>
-     
-    <div  class="user-box">
-      <input
-        type="email"
-        name="email"
-        placeholder="Email..."></input>
-      </div>
-      
-      
-      <button class="button2"
-        type="submit" >
-        Search
-        </button>
-        </div>
-        </div>
 );
 }
